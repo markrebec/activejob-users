4b3b51e72368b88059fd9433ab1f4325c194222d
Frank <fbelanger@ackroo.com>
2018-11-23 21:14:01 -0500

    coverage

--- spec/active_job/users_spec.rb
+++ spec/active_job/users_spec.rb
@@ -16,9 +16,30 @@ RSpec.describe ActiveJob::Users do
       subject.arguments << {foo: 'bar'}
       expect(subject.arguments.last).to eq({foo: 'bar'})
     end
+
+    it 'serialize the job_user' do
+      subject.job_user = user
+      expect(subject.serialize["job_user"]).to eq(user)
+    end
+
+    it 'deserialize the job_user' do
+      subject.job_user = user
+      job = TestJob.deserialize(subject.serialize)
+      expect(job.job_user).to eq(user)
+    end
+
+    it 'passes job_user in enqueue' do
+      subject.enqueue(job_user: user)
+      expect(subject.job_user).to eq(user)
+    end
+
+    it 'passes job_user in set' do
+      expect_any_instance_of(TestJob).to receive(:job_user)
+      TestJob.set(job_user: user).perform_later
+    end
   end
 
-  context 'when a job_user argument is not passed to the job' do
+  context 'when a job_user is not passed to the job' do
     it 'sets the job_user to nil' do
       expect(subject.job_user).to eq(nil)
     end
@@ -27,10 +48,28 @@ RSpec.describe ActiveJob::Users do
       subject.arguments << {foo: 'bar'}
       expect(subject.arguments.last).to eq({foo: 'bar'})
     end
+
+    it 'serialize the job_user' do
+      expect(subject.serialize["job_user"]).to eq(nil)
+    end
+
+    it 'deserialize the job_user' do
+      job = TestJob.deserialize(subject.serialize)
+      expect(job.job_user).to eq(nil)
+    end
+
+    it 'passes job_user in enqueue' do
+      subject.enqueue(job_user: nil)
+      expect(subject.job_user).to eq(nil)
+    end
+
+    it 'passes job_user in set' do
+      expect_any_instance_of(TestJob).to receive(:job_user)
+      TestJob.perform_later
+    end
   end
 
   it 'respond_to job_user' do
     expect(subject).to respond_to(:job_user)
-    TestJob.perform_now
   end
 end
